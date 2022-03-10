758426509e737049d944a4fc4cca0148deea467a
Jerônimo <jeronimoaa@al.insper.edu.br>
2022-03-10 16:21:52 -0300

    classe Datagram e Message finalizadas

--- Projeto 3/datagrama.py
+++ Projeto 3/datagrama.py
@@ -19,37 +19,41 @@ class Datagram():
     HEAD_TIPS_BYTES = [b'\xAA'] * 2
     EOP_BYTES = [b'\xBB'] * 4
 
-    def __init__(self, number, ammount, payload):
+    # especificações do datagrama para referenciamento externo
+    MAX_PAYLOAD = 114
+    MAX_AMMOUNT = 256 ** 3
+
+    def __init__(self, ammount, number, payload):
         '''
         Inicializa um único datagrama pertencente a um grupo de datagramas (pacote)
 
         Parâmetros:
-        - number: número do datagrama (int) (o número 1 é o primeiro)
         - ammount: número total de datagramas (int)
+        - number: número do datagrama (int) (o número 1 é o primeiro)
         - payload: dados (lista de bytes, no máximo 114 bytes)
 
         '''
 
         # verifica parâmetros inválidos
-        if ammount > 256 ** 3: raise ValueError('Impossível enviar tantos pacotes')
+        if ammount > Datagram.MAX_AMMOUNT: raise ValueError('Impossível enviar tantos pacotes')
         if number > ammount: raise ValueError('Número do pacote maior que o número total de pacotes')
-        if len(payload) > 114: raise ValueError('Payload grande demais')
+        if len(payload) > Datagram.MAX_PAYLOAD: raise ValueError('Payload grande demais')
 
         # amarra os argumentos ao objeto
-        self.number = number
         self.ammount = ammount
+        self.number = number
         self.payload = payload
 
         # conversão de number e ammount para 3 bytes cada
-        number_bytes = number.to_bytes(3, byteorder="big")
         ammount_bytes = ammount.to_bytes(3, byteorder="big")
+        number_bytes = number.to_bytes(3, byteorder="big")
 
         # separação dos bytes
-        number_bytes = utils.split_bytes(number_bytes)
         ammount_bytes = utils.split_bytes(ammount_bytes)
+        number_bytes = utils.split_bytes(number_bytes)
 
         # junção das listas de bytes que formam o HEAD e o datagrama completo
-        self.head = Datagram.HEAD_ENDS + ammount_bytes + number_bytes + Datagram.HEAD_ENDS
+        self.head = Datagram.HEAD_TIPS_BYTES + ammount_bytes + number_bytes + Datagram.HEAD_TIPS_BYTES
         self.bytes = self.head + self.payload + Datagram.EOP_BYTES
         
 
--- Projeto 3/mensagem.py
+++ Projeto 3/mensagem.py
@@ -3,12 +3,21 @@ Arquivo criado para armazenar a classe de Datagrama
 
 '''
 
+import math
 import utils
 
 from datagrama import Datagram
 
 class Message():
-    ''' Classe que representa uma mensagem a ser enviada dividida em datagramas '''
+    '''
+
+    Classe que representa uma mensagem a ser enviada dividida em datagramas
+
+    Propriedades:
+    - datagrams: lista de Datagrams que compõe a mensagem
+    - bytes: lista de bytes que compõem a mensagem finalizada (é o que é transmitido)
+
+    '''
 
     def __init__(self, raw_data):
         '''
@@ -19,17 +28,43 @@ class Message():
         
         '''
 
+        # extrai as especificações do datagrama
+        max_ammount = Datagram.MAX_AMMOUNT
+        max_payload = Datagram.MAX_PAYLOAD
+
         # verifica a validade dos argumentos
-        if len(raw_data) > 256 ** 3 * 114: raise ValueError('Mensagem grande demais')
+        if len(raw_data) > max_ammount * max_payload: raise ValueError('Mensagem grande demais')
 
-        # divide os dados brutos em uma lista de payloads
-        self.size_datagrams = round(len(raw_data) / 114)
-        if len(raw_data) % 114 > 0: self.size_datagrams += 1
+        # calcula a quantidade de datagramas necessários
+        number_of_datagrams = math.ceil(len(raw_data) / max_payload)
 
-        # mensagem dividida em datagramas
+        # divide os dados brutos em datagramas  --- --- --- --- --- --- --- --- --- --- ---
+        # e cria a lista de bytes da mensagem
         self.datagrams = list()
+        self.bytes = list()
 
-        for i in range(self.size_datagrams):
-            payload = 
-            datagram = Datagram(i + 1, self.size_datagrams, )
+        for i in range(number_of_datagrams):
+
+            # intervalo para extrai dos dados brutos
+            from_index = i * max_payload
+            to_index = (i + 1) * max_payload
+
+            # extrai o intervalo e cria um datagrama com o payload
+            payload = raw_data[from_index:to_index]
+            datagram = Datagram(number_of_datagrams, i + 1, payload)
+
+            # adiciona o datagrama à lista de datagramas e preenche a lista de bytes
+            self.datagrams.append(datagram)
+            self.bytes += datagram.bytes
+
+        #-- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+
+
+
+
 
