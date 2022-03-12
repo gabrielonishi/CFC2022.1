ac8fd98902b788a6861d4a277d7891188da9811e
Jerônimo <jeronimoaa@al.insper.edu.br>
2022-03-12 14:53:42 -0300

    Inclusão das mudanças feitas e mensagem e pacote4

--- Projeto 3/mensagem.py
+++ Projeto 3/mensagem.py
@@ -19,10 +19,12 @@ class Message():
     - data_size: tamanho dos dados úteis a serem enviados
     - total_size: tamanho de todos os pacotes somados
     - bytes: lista de bytes dos dados separados em pacotes
+    - number_of_packets: quantidade de pacotes que compõem a mensagem
+    - is_complete: se a mensagem está completa ou não
 
     '''
 
-    def __init__(self, data):
+    def __init__(self, data=[]):
         '''
         Inicializa um objeto Message
 
@@ -34,30 +36,40 @@ class Message():
         # amarra o argumento à instância de Message
         self.data = data
 
+        # determina se a mensagem está completa ou não
+        if len(data) == 0:
+            self.last_received = 0
+            self.is_complete = False
+        else:
+            self.last_received = False
+            self.is_complete = True
+
         # extrai as especificações dos pacotes
         max_data = Packet.MAX_DATA
-        max_payload = Packet.MAX_PAYLOAD
+        payload_size = Packet.PAYLOAD_SIZE
 
         # verifica a validade dos argumentos
         if len(data) > max_data: raise ValueError('Mensagem grande demais')
 
         # calcula a quantidade de datagramas necessários
-        number_of_datagrams = math.ceil(len(data) / max_payload)
+        number_of_packets = math.ceil(len(data) / payload_size)
+        if number_of_packets > Packet.MAX_PACKETS: raise ValueError('Impossível enviar tantos pacotes')
+        self.number_of_packets = number_of_packets
 
         # divide os dados brutos em pacotes  --- --- --- --- --- --- --- --- --- --- ---
         # e adiciona os bytes dos pacotes à lista de bytes
         self.packets = dict()
         self.bytes = list()
 
-        for i in range(number_of_datagrams):
+        for i in range(number_of_packets):
 
-            # intervalo para extrai dos dados brutos
-            from_index = i * max_payload
-            to_index = (i + 1) * max_payload
+            # intervalo para extrair dos dados brutos
+            from_index = i * payload_size
+            to_index = (i + 1) * payload_size
 
             # extrai o intervalo e cria um datagrama com o payload
             payload = data[from_index:to_index]
-            packet = Packet(self, number_of_datagrams, i + 1, payload)
+            packet = Packet(self, i + 1, payload)
 
             # adiciona o datagrama à lista de datagramas e preenche a lista de bytes
             self.packets[i + 1] = packet
@@ -70,7 +82,43 @@ class Message():
         self.data_size = len(self.data)
 
 
+    def receive_packet(self, raw_packet):
+        '''
+        Adiciona um pacote recebido à mensagem
+        Retorna False se o pacote recebido for inválido
+
+        Parâmetros:
+        - raw_packet: lista de bytes do pacote recebido
+        
+        '''
+
+        if self.is_complete: raise TypeError('Essa mensagem já está completa')
+
+        # decodifica o pacote e retorna False se houver falhas
+        received_packet = Packet.decode(self, raw_packet)
+        if received_packet == False: return False
+
+        # verifica se o recebido é o próximo do último recebido
+        if received_packet.number != self.last_received + 1: return False
+
+        # verifica se a mensagem foi completada
+        if self.last_received == self.number_of_packets: self.is_complete = True
+
+        # adiciona os dados do pacote aos dados totais da mensagem
+        self.packets[received_packet.number] = received_packet
+        self.bytes += received_packet.bytes
+        self.data += received_packet.data
+        
+        return received_packet
 
 
+d_out = [b'\xFF'] * 2 ** 10
+m_out = Message(d_out)
 
+m_in = Message()
 
+for i in range(1, m_out.number_of_packets + 1):
+    packet = m_out.packets[i].bytes
+    m_in.receive_packet(packet)
+
+print(m_in.data == m_out.data)
--- Projeto 3/pacote.py
+++ Projeto 3/pacote.py
@@ -9,8 +9,8 @@ class Packet():
     '''
     Representa um tipo de pacote definido pelo datagrama especificado no enunciado
 
-    10 bytes - HEAD: AA AA {qtde total de datagramas} {número do datagrama} AA AA
-    1 a 114 bytes - PAYLOAD
+    10 bytes - HEAD: AA AA 2{qtde total de datagramas} 2{número do datagrama} {tamanho dos dados} AA AA AA
+    114 bytes - PAYLOAD
     4 bytes - EOP: BB BB BB BB
 
     Propriedades:
@@ -18,7 +18,7 @@ class Packet():
     - message: objeto Message ao qual o pacote pertence
     - ammount: quantidade de pacotes
     - number: identidade do pacote
-    - payload: dados do pacote
+    - data: dados do pacote
     - head: lista de bytes que compõem o HEAD
     - bytes: lista de bytes pronta para envio
     - data_size: tamanho do payload em bytes
@@ -26,64 +26,126 @@ class Packet():
 
     '''
 
-    # especificações do datagrama para referenciamento
-    HEAD_SIZE = 10                          # tamanho do HEAD em bytes
-    MAX_PAYLOAD = 114                       # tamanho do payload em bytes
+    # especificações do datagrama para referenciamento  --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    HEAD_START_SIZE = 2                     # quantidade de bytes que compõem o início do HEAD
+    NUMBER_SIZE = 2                         # tamanho do número do pacote em bytes
+    SIZE_INDICATOR_SIZE = 1                 # tamanho do número que indica o tamanho do payload
+    HEAD_END_SIZE = 3                       # quantidade de bytes que compõem o fim do HEAD
+    PAYLOAD_SIZE = 114                      # tamanho do payload em bytes
     EOP_SIZE = 4                            # tamanho do EOP em bytes
-    NUMBER_SIZE = 3                         # tamanho do número do datagrama em bytes
-    MAX_AMMOUNT = 256 ** NUMBER_SIZE        # quantidade máxima de datagramas em uma mensagem
-    MAX_DATA = MAX_AMMOUNT * MAX_PAYLOAD    # quantidade máxima de bytes úteis
-    HEAD_TIPS_SIZE = ((HEAD_SIZE - NUMBER_SIZE * 2) / 2)    # tamanho das "bandeiras" do HEAD
-    MAX_SIZE = HEAD_SIZE + MAX_PAYLOAD + EOP_SIZE           # tamanho máximo do datagrama inteiro
 
-    # verifica a validade das especificações
-    if int(HEAD_TIPS_SIZE) != HEAD_TIPS_SIZE:
-        raise ValueError('Especificações do datagrama inválidas')
-    HEAD_TIPS_SIZE = int(HEAD_TIPS_SIZE)
+    #   --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    # especificações derivadas  --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    HEAD_SIZE =  HEAD_START_SIZE + NUMBER_SIZE * 2 + SIZE_INDICATOR_SIZE + HEAD_END_SIZE
+    MAX_PACKETS = 256 ** NUMBER_SIZE                        # quantidade máxima de pacotes em uma mensagem
+    MAX_DATA = MAX_PACKETS * PAYLOAD_SIZE                   # quantidade máxima de bytes úteis
+    PACKET_SIZE = HEAD_SIZE + PAYLOAD_SIZE + EOP_SIZE       # tamanho de um único pacote
+    #   --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
     # bytes de sinalização de início e fim do HEAD e EOP
-    HEAD_TIPS_BYTES = [b'\xAA'] * int((HEAD_SIZE - NUMBER_SIZE * 2) / 2)
+    PAYLOAD_FILLER = [b'\x55']
+    HEAD_START_BYTES = [b'\xAA'] * HEAD_START_SIZE
+    HEAD_END_BYTES = [b'\xAA'] * HEAD_END_SIZE
     EOP_BYTES = [b'\xBB'] * EOP_SIZE
 
-    def __init__(self, message, ammount, number, payload):
+    # HEAD genérico para comparação com HEADs de pacotes recebidos
+    SAMPLE_HEAD = HEAD_START_BYTES + ['ammount'] * NUMBER_SIZE + ['number'] * NUMBER_SIZE + ['size'] * SIZE_INDICATOR_SIZE + HEAD_END_BYTES
+
+    def __init__(self, message, number, data):
         '''
         Inicializa um único pacote pertencente a uma mensagem
 
         Parâmetros:
         - message: objeto Message para agrupar datagramas relacionados
-        - ammount: número total de datagramas (int)
         - number: número do datagrama (int) (o número 1 é o primeiro)
-        - payload: dados (lista de bytes, no máximo 114 bytes)
+        - data: dados (lista de bytes, no máximo 114 bytes)
 
         '''
 
+        # cria uma variável para o tamanho do payload e extrai o tamanho da mensagem em pacotes
+        data_size = len(data)
+        ammount = message.number_of_packets
+
         # verifica parâmetros inválidos
-        if ammount > Packet.MAX_AMMOUNT: raise ValueError('Impossível enviar tantos pacotes')
         if number > ammount: raise ValueError('Número do pacote maior que o número total de pacotes')
-        if len(payload) > Packet.MAX_PAYLOAD: raise ValueError('Payload grande demais')
+        if data_size > Packet.PAYLOAD_SIZE: raise ValueError('Payload grande demais')
 
         # amarra os argumentos ao objeto
         self.message = message
         self.ammount = ammount
         self.number = number
-        self.payload = payload
+        self.data = data
+        self.data_size = data_size
 
         # conversão de number e ammount para 3 bytes cada
+        data_size_bytes = data_size.to_bytes(Packet.SIZE_INDICATOR_SIZE, byteorder="big")
         ammount_bytes = ammount.to_bytes(Packet.NUMBER_SIZE, byteorder="big")
         number_bytes = number.to_bytes(Packet.NUMBER_SIZE, byteorder="big")
+        data_size_bytes = utils.split_bytes(data_size_bytes)
         ammount_bytes = utils.split_bytes(ammount_bytes)
         number_bytes = utils.split_bytes(number_bytes)
 
+        # preenchimento do payload
+        self.payload = self.data
+        while len(self.payload) < Packet.PAYLOAD_SIZE:
+            self.payload += Packet.PAYLOAD_FILLER
+
         # junção das listas de bytes que formam o HEAD e o pacote completo
-        self.head = Packet.HEAD_TIPS_BYTES + ammount_bytes + number_bytes + Packet.HEAD_TIPS_BYTES
+        self.head = Packet.HEAD_START_BYTES + ammount_bytes + number_bytes + data_size_bytes + Packet.HEAD_END_BYTES
         self.bytes = self.head + self.payload + Packet.EOP_BYTES
 
         # definição de demais propriedades
-        self.data_size = len(self.payload)
         self.total_size = len(self.bytes)
     
 
+    @staticmethod
+    def decode(message, raw_packet):
+        '''
+        Retorna um objeto Datagram a partir de uma lista de bytes de um datagrama recebido
+        Retorna False se receber um pacote com especificações inesperadas
         
+        Deve ser chamada somente pelo método receive_packet da classe Message
+
+        Parâmetros:
+        - message: instância de Message à qual o datagrama será atribuido
+        - raw_datagram: dados brutos que seguem o formato do datagrama
+
+        '''
+
+        # extrai o HEAD e o EOP
+        head = raw_packet[ : Packet.HEAD_SIZE]
+        eop = raw_packet[-1 * Packet.EOP_SIZE : ]
+
+        # verifica a validade do HEAD e extrai os dados do HEAD --- --- --- --- --- ---
+        for i in range(Packet.HEAD_SIZE):
+
+            expected_value = Packet.SAMPLE_HEAD[i]
+            received_byte = head[i]
+
+            # lida com bytes variáveis
+            if isinstance(expected_value, str):
+                if expected_value == 'ammount': ammount = int.from_bytes(received_byte, 'big'); continue
+                if expected_value == 'number': number = int.from_bytes(received_byte, 'big'); continue
+                if expected_value == 'size': data_size = int.from_bytes(received_byte, 'big'); continue
+
+            # lida com bytes fixos
+            else:
+                if received_byte != expected_value: return False
+        #   --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+        # verifica a validade do EOP
+        if eop != Packet.EOP_BYTES: return False
+
+        # extrai o payload
+        payload = raw_packet[Packet.HEAD_SIZE : -1 * Packet.EOP_SIZE]
+        data = payload[ : data_size]
+        
+        # cria a nova instância de Datagrama
+        message.number_of_packets = ammount
+        packet = Packet(message, number, data)
+
+        return packet
 
 
 
